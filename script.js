721b684389f880557f4ff9502d3a86e91477385d
retr0lbb <retr0lbb@gmail.com>
2022-11-23 17:23:39 +0000

    all

---
+++ script.js
@@ -0,0 +1,4 @@
+//ganhei do guilherme no dia 23/11/2022
+//ganhei do guilherme no dia 23/11/2022 de novo
+//ganhei do guilherme no dia 23/11/2022 de novo, mais uma vez
+//ganhei do guilherme no dia 23/11/2022 de novo, mais uma vez, novamente
